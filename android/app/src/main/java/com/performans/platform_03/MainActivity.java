0b4b3476bbbcc97ed71286e7785cc666ce4ece11
ilker <ilker@okutman.com>
2023-02-14 18:03:53 +0300

    switch to java from kotlin

---
+++ android/app/src/main/java/com/performans/platform_03/MainActivity.java
@@ -0,0 +1,6 @@
+package com.performans.platform_03;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/performans/platform_03/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.performans.platform_03
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
